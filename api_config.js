6f8755aa34c934d96fbc412ece2386c0c71212f2
edufmsc <edu.fmsc@gmail.com>
2026-07-24 12:05:23 +0800

    Update api_config.js

--- api_config.js
+++ api_config.js
@@ -1,12 +1,12 @@
 /**
- * 月考核系統 V4｜GitHub 前端設定
+ * 月考核系統｜GitHub 前端設定
  *
  * 只要修改 API_URL。
  * 請貼入 Apps Script「正式部署」且結尾為 /exec 的網址。
  * 不要使用 /dev，也不要使用 script.googleusercontent.com 的跳轉網址。
  */
 window.V3_CONFIG = Object.freeze({
-  APP_NAME: '月考核系統 V4',
+  APP_NAME: '月考核系統',
   APP_VERSION: '1.0.1',
   API_URL: 'https://script.google.com/macros/s/AKfycbw2Pvh2S0kOlpiNVOuF87njlMD8fUEF40WYIuE_OONKW4FOwjFBCwW6fiL18W-G8r4/exec',
   REQUEST_TIMEOUT_MS: 30000,
