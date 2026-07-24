e5e43fd602c70c6ee927e4c2df9b02eea6efb544
edufmsc <edu.fmsc@gmail.com>
2026-07-24 08:48:40 +0800

    Create api_config.js for 月考核系統 V3
    Added configuration for 月考核系統 V3 with API URL.

---
+++ api_config.js
@@ -0,0 +1,14 @@
+/**
+ * 月考核系統 V3｜GitHub 前端設定
+ *
+ * 只要修改 API_URL。
+ * 請貼入 Apps Script「正式部署」且結尾為 /exec 的網址。
+ * 不要使用 /dev，也不要使用 script.googleusercontent.com 的跳轉網址。
+ */
+window.V3_CONFIG = Object.freeze({
+  APP_NAME: '月考核系統 V3',
+  APP_VERSION: '1.0.1',
+  API_URL:'https://script.google.com/macros/s/AKfycbxf7aaodz2V7F5EWnKd3FlI0KMEGzaGw1XPMVg6Mw7ynPW078YksIHlZt2ljouAp_Lo/exec',
+  REQUEST_TIMEOUT_MS: 30000,
+  SESSION_STORAGE_KEY: 'monthlyEvaluationV3Session'
+});
